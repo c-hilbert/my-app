6d5714410a0660308e2fea998adff2a76ba06ab7
Carmen Hilbert <carminaburana@gmail.com>
2024-07-25 21:47:36 -0400

    Updated database connection for Heroku

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '', // Use the password you set for your MySQL root user
-  database: 'pharmacy_db',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+let pool;
+
+if (process.env.JAWSDB_URL) {
+  // If running on Heroku with JawsDB
+  pool = mysql.createPool(process.env.JAWSDB_URL);
+} else {
+  // If running locally
+  pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '', // Use the password you set for your MySQL root user
+    database: 'pharmacy_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+}
 
 module.exports = pool.promise();
